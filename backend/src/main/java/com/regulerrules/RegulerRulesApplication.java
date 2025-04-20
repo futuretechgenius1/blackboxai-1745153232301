6827ee6a132b6043c3d712d12eff889d6b16253c
futuretechgenius1 <manoharyadavit@gmail.com>
2025-04-20 18:17:33 +0530

    Add backend/src/main/java/com/regulerrules/RegulerRulesApplication.java 1745153253063

---
+++ backend/src/main/java/com/regulerrules/RegulerRulesApplication.java
@@ -0,0 +1,12 @@
+package com.regulerrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegulerRulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegulerRulesApplication.class, args);
+    }
+}
